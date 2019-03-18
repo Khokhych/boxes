1e1a0e147d3a26a06015149e7561b323f1490303
KHOKHYCH <bohdan.khokhych@gmail.com>
2019-03-18 17:12:05 +0200

    add boxes

--- main.js
+++ main.js
@@ -11,30 +11,32 @@ class boxCreator  {
         top:${position[0]}px;
         left:${position[1]}px;
         `;
-        console.log(BGcolor);
         box.style.cssText = styleBox;
         document.querySelector('body').appendChild(box);
     }
 };
 
 let boxGenerator = function() {
-
-    for (let i = 0; i <= 100; i ++){
+    let windowWidth = document.documentElement.clientWidth;
+    let windowHeight = document.documentElement.clientHeight;
+    for (let i = 0; i <= 1000; i ++){
         let rundon1 = Math.round(Math.random() * 100) + 1;
         let rundon2 = Math.round(Math.random() * 100) + 1;
         let rundon3 = Math.round(Math.random() * 100);
-        let positionTop = Math.random() * (1000 - 0) + 0;
-        let positionLeft = Math.random() * (1000 - 0) + 0;
+        let positionTop = Math.random() * (windowHeight - rundon2);
+        let positionLeft = Math.random() * (windowWidth - rundon1);
+        setTimeout(() => {
+            new boxCreator(
+                `rgba(
+                    ${rundon1},
+                    ${rundon2},
+                    ${rundon3},
+                    ${+(rundon1 /100).toFixed(1)})`
+                ,[rundon1,rundon2]
+                ,[positionTop,positionLeft]
+                );
+        }, 1500);
 
-        new boxCreator(
-            `rgba(
-                ${rundon1},
-                ${rundon2},
-                ${rundon3},
-                ${+(rundon1 /100).toFixed(1)})`
-            ,[rundon1,rundon2]
-            ,[positionTop,positionLeft]
-            );
     
     }
 };
